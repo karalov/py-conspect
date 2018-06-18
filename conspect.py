52cbf9a07b7c9eff5574da086993d03b5f724551
karalov <karalov.d@gmail.com>
2018-06-18 16:10:50 -0400

    new conspects

--- conspect.py
+++ conspect.py
@@ -88,3 +88,20 @@ for k,c in groupby(l):
 l=[('a', 2), ('e', 3), ('c', 3), ('d', 1), ('b', 1)]
 sorted(l,key=lambda x: (-x[1],x[0]))
 #[('c', 3), ('e', 3), ('a', 2), ('b', 1), ('d', 1)] - first by number desc, then by letter asc (deflt)  
+
+my_mapping = {'a': 23, 'b': 42, 'c': 0xc0ffee}
+my_mapping
+{'b': 42, 'c': 12648430. 'a': 23}  # 😞
+
+# The "json" module can do a much better job:
+import json
+print(json.dumps(my_mapping, indent=4, sort_keys=True))
+{
+    "a": 23,
+    "b": 42,
+    "c": 12648430
+}
+
+# Note this only works with dicts containing
+# primitive types (check out the "pprint" module):
+>>> json.dumps({all: 'yup'})
